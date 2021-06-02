15114b7220dfd59d002e8afa7ef0b5f79e638014
Eugene <ek488@cornell.edu>
2021-06-01 20:26:03 -0400

    R and UL cards can no longer be the same

--- card_downloader.py
+++ card_downloader.py
@@ -4,7 +4,7 @@ from urllib.request import Request, urlopen
 
 #custom_cardname_set = {"Ballista Squad", "Black Brachios", "Book of Moon", "Breakthrough Skill", "Compulsory Evacuation Device", "Dust Knight", "Fabled Soulkius", "Giant Rat", "Kelbek", "Mecha Phantom Beast Blackfalcon", "Miracle's Wake", "Poseidon Wave", "Raigeki Break", "Tackle Crusader", "Upstart Goblin", "Winged Rhynos"}
 
-custom_cardname_set = {"Book of Moon"}
+custom_cardname_set = {"Ballista Squad", "Book of Lunar Eclipse", "Book of Moon", "Breakthrough Skill"}
 
 def get_ygoprodeck_json(cardname):
     req = Request("https://db.ygoprodeck.com/api/v7/cardinfo.php?name=" + cardname.replace(" ", "%20"), headers={'User-Agent': 'Mozilla/5.0'})
@@ -28,23 +28,23 @@ def download_img(cardname):
     ygoprodeck_json = get_ygoprodeck_json(cardname)
     card_id = get_id_from_json(ygoprodeck_json)
     image_filename = str(card_id) + ".jpg"
-    if not path.exists("card_images/" + image_filename):
+    if not path.exists("public/images/card_images/" + image_filename):
         image_url = get_image_url_from_json(ygoprodeck_json)
-        urllib.request.urlretrieve(image_url, "card_images/" + image_filename)
-        print(cardname + " image downloaded.")
+        urllib.request.urlretrieve(image_url, "public/images/card_images/" + image_filename)
+        print(cardname + "(" + str(card_id) + ") image downloaded.")
         time.sleep(5)
 
 def download_img_from_id(card_id):
     image_filename = str(card_id) + ".jpg"
-    if not path.exists("card_images/" + image_filename):
+    if not path.exists("public/images/card_images/" + image_filename):
         image_url = get_image_url_from_json(ygoprodeck_json)
-        urllib.request.urlretrieve(image_url, "card_images/" + image_filename)
-        print(cardname + " image downloaded.")
+        urllib.request.urlretrieve(image_url, "public/images/card_images/" + image_filename)
+        print(cardname + " (" + str(card_id) + ") image downloaded.")
         time.sleep(2)
 
-#def download_imgs(cardname_set):
-    #for cardname in cardname_set:
-        #download_img(cardname)
+def download_imgs(cardname_set):
+    for cardname in cardname_set:
+        download_img(cardname)
 
 #def download_json(cardname: str):
     #json_filename = cardname + ".json"
@@ -58,4 +58,4 @@ def download_img_from_id(card_id):
     #for cardname in cardname_set:
         #download_json(cardname)
 
-#download_imgs(custom_cardname_set)
+download_imgs(custom_cardname_set)
